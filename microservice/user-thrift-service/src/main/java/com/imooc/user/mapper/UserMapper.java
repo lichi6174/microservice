cda851293d82a090f8104115a9fdbe5fa05a121c
test <test@shebaodai.cn>
2018-06-06 22:31:38 +0800

    修改user-thrift-service mapper UserMapper

--- microservice/user-thrift-service/src/main/java/com/imooc/user/mapper/UserMapper.java
+++ microservice/user-thrift-service/src/main/java/com/imooc/user/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package com.imooc.user.mapper;
 
 import com.imooc.thrift.user.UserInfo;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
@@ -8,9 +9,16 @@ import org.apache.ibatis.annotations.Select;
 @Mapper
 public class UserMapper {
 
-    @Select("select id,username,password,real_name as realName," +
+    @Select("select id,username, password, real_name as realName," +
             "mobile, email from pe_user where id=#{id}")
     UserInfo getUserById(@Param("id")int id);
 
+    @Select("select id,username, password, real_name as realName," +
+            "mobile, email from pe_user where username=#{username}")
+    UserInfo getUserByName(@Param("username")String username);
+
+    @Insert("insert into pe_user (username, password, real_name, mobile, email)" +
+            "values (#{u.username}, #{u.password}, #{u.realName}, #{u.mobile}, #{u.email})")
+    void registerUser(@Param("u") UserInfo userInfo);
 
 }

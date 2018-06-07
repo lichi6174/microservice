5ef702ad3539998694dec8266aac958535777049
test <test@shebaodai.cn>
2018-06-07 16:21:55 +0800

    come on ......

--- microservice/user-thrift-service/src/main/java/com/imooc/user/mapper/UserMapper.java
+++ microservice/user-thrift-service/src/main/java/com/imooc/user/mapper/UserMapper.java
@@ -7,13 +7,13 @@ import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
-public class UserMapper {
+public interface UserMapper {
 
-    @Select("select id,username, password, real_name as realName," +
+    @Select("select id, username, password, real_name as realName," +
             "mobile, email from pe_user where id=#{id}")
     UserInfo getUserById(@Param("id")int id);
 
-    @Select("select id,username, password, real_name as realName," +
+    @Select("select id, username, password, real_name as realName," +
             "mobile, email from pe_user where username=#{username}")
     UserInfo getUserByName(@Param("username")String username);
 
